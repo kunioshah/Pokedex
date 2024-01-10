5d9cba2258eff9ca064dbfb0bd03cb4c931efb62
kunioshah <kunioshah@gmail.com>
2024-01-09 22:58:21 -0800

    Added scripts to deploy to github pages and remove node_modules

--- src/Root.js
+++ src/Root.js
@@ -1,31 +1,19 @@
-import React from 'react'
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes
-} from "react-router-dom";
-import App from './App';
-import PokeDetails from './components/PokeDetails';
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import PokeDetails from "./components/PokeDetails";
 
 const Root = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route 
-                  path="/pokemon/"
-                  element={<PokeDetails/>}
-                />
-                <Route 
-                  path="/pokemon/:id"
-                  element={<PokeDetails/>}
-                />
-                <Route 
-                  path="/"
-                  element={<App/>}
-                />
-            </Routes>
-        </Router>
-    )
-} 
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/pokemon/" element={<PokeDetails />} />
+        <Route exact path="/pokemon/:id" element={<PokeDetails />} />
+        <Route exact path="/Pokedex" element={<App />} />
+        <Route exact path="/" element={<App />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default Root
+export default Root;
